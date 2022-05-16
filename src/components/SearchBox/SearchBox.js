964d37913ab53f034d899d23e24f0e0324eae094
RashadGuluzadeh <rashadquluzadegmail.com>
2022-05-16 17:23:35 +0400

    Project

---
+++ src/components/SearchBox/SearchBox.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+import './SearchBox.css';
+import { API_KEY } from "./shortened"
+
+class SearchBox extends Component {
+    state = {
+        searchLine: ''
+    }
+    searchLineChangeHandler = (e) => {
+        this.setState({ searchLine: e.target.value });
+    }
+    searchBoxSubmitHandler = (e) => {
+        e.preventDefault();
+    }
+    search() {
+        console.log(this.state.searchLine)
+        let url = `https://www.omdbapi.com/?s=${ this.state.searchLine}&apikey=${ API_KEY }`
+        fetch(url, {
+            method : 'GET',
+        }).then(response => response.json())
+        .then(data => console.log(data.Search))
+    }
+    render() {
+        const { searchLine } = this.state;
+
+        return (
+            <div className="search-box">
+                <form className="search-box__form" onSubmit={this.searchBoxSubmitHandler}>
+                    <label className="search-box__form-label">
+                        Искать фильм по названию:
+                        <input
+                            value={searchLine}
+                            type="text"
+                            className="search-box__form-input"
+                            placeholder="Например, Shawshank Redemption"
+                            onChange={this.searchLineChangeHandler}
+                        />
+                    </label>
+                    <button
+                        type="submit"
+                        className="search-box__form-submit"
+                        disabled={!searchLine}
+                        onClick={() => this.search()}
+                    >
+                        Искать
+                    </button>
+                </form>
+            </div>
+        );
+    }
+}
+ 
+export default SearchBox;
